240371e2d200c6197db719c6ef615806e0ab3f28
labmember003 <93467641+labmember003@users.noreply.github.com>
2023-09-09 11:28:01 +0530

    fixed api http issue

--- app/src/main/java/com/labmember003/farmercompanion/MainActivity2.kt
+++ app/src/main/java/com/labmember003/farmercompanion/MainActivity2.kt
@@ -43,21 +43,6 @@ class MainActivity2 : AppCompatActivity() {
 
 
         setContentView(R.layout.activity_main2)
-/*
-        val a = listOf(R.id.year1, R.id.year2, R.id.year3, R.id.year4)
-        a.forEach{
-            findViewById<ImageView>(it).setOnClickListener {
-                val intent = Intent(this, MainActivity::class.java)
-                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
-                when (it) {
-                    R.id.year1 ->  intent.putExtra("crop", "rice")
-                }
-                //intent.putExtra("crop", "rice")
-                startActivity(intent)
-            }
-        }
-
- */
         findViewById<ImageView>(R.id.settingsImage).setOnClickListener {
             val intent = Intent(this, SettingsActivity::class.java)
             startActivity(intent)
@@ -84,12 +69,6 @@ class MainActivity2 : AppCompatActivity() {
             startActivity(intent)
         }
 
-
-
-
-
-
-        // BOTTOM NAVIGATION
         findViewById<ImageView>(R.id.nv1).setOnClickListener {
             val intent = Intent(this, MainActivity2::class.java)
             startActivity(intent)
